3822a2051b5da26371d6b4a1db5aca48d4a13f33
Kavana M.K <37045668+kavanamk@users.noreply.github.com>
2018-12-21 09:48:56 +0530

    Add files via upload

---
+++ lsa.py
@@ -0,0 +1,38 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+
+# Sample Data
+dataset = ["The amount of polution is increasing day by day",
+           "The concert was just great",
+           "I love to see Gordon Ramsay cook",
+           "Google is introducing a new technology",
+           "AI Robots are examples of great technology present today",
+           "All of us were singing in the concert",
+           "We have launch campaigns to stop pollution and global warming"]
+
+dataset = [line.lower() for line in dataset]
+
+# Creating Tfidf Model
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(dataset)
+
+# Visualizing the Tfidf Model
+print(X[0])
+
+
+# Creating the SVD
+lsa = TruncatedSVD(n_components = 4, n_iter = 100)
+lsa.fit(X)
+
+
+# First Column of V
+row1 = lsa.components_[0]
+
+# Creating the SVD
+lsa = TruncatedSVD(n_components = 4, n_iter = 100)
+lsa.fit(X)
+
+
+# First Column of V
+row1 = lsa.components_[3]
+
